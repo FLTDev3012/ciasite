9197046900dad8c8bfcd85d86dd4b4b6884d862f
dorian fiolet <dorian3012@hotmail.fr>
2024-07-26 21:37:05 +1000

    services traits done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home, :nosservices]
 
   def home
   end
+
+  def nosservices
+  end
+
 end
