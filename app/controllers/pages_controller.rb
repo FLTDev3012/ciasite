d4a7ca0f83cdb70e5b6531a7df47dc73ada1b4e5
dorian fiolet <dorian3012@hotmail.fr>
2024-08-12 19:01:20 +1000

    save

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :nosservices]
+  skip_before_action :authenticate_user!, only: [:home, :nosservices, :projects]
 
   def home
   end
 
   def nosservices
+  end
+
+  def projects
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'nosservices', to: 'pages#nosservices'
+  get 'projects', to: 'pages#projects'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
