86779865506f4897e1fe642fcc8c67dba9ac115b
dorian fiolet <dorian3012@hotmail.fr>
2024-09-10 16:49:48 +1000

    fistcat done petit pb sur select

--- app/javascript/controllers/estimation_controller.js
+++ app/javascript/controllers/estimation_controller.js
@@ -2,20 +2,70 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="estimation"
 export default class extends Controller {
-  static targets = [ "firstCatFirstType", "deuxiemecrit" ]
+  static targets = [ "firstCatFirstType", "firstCatSecondType", "firstCatThirdType" ]
   connect() {
     console.log("estimation controller connecté!")
   }
 
   first_cat_first_choice() {
 
+    // add color good type
+    this.firstCatFirstTypeTargets.forEach((target) => {
+      target.classList.add("estimation-selected");
+    });
 
+    // remove color bad type
 
-    //console.log("click!")
-    //this.firstCatFirstTypeTarget.classList.add("teston");
+    this.firstCatSecondTypeTargets.forEach((target) => {
+      target.classList.remove("estimation-selected");
+    });
+
+    this.firstCatThirdTypeTargets.forEach((target) => {
+      target.classList.remove("estimation-selected");
+    });
+
+  }
+
+  first_cat_second_choice() {
+
+    // add color good type
 
     this.firstCatFirstTypeTargets.forEach((target) => {
-      target.classList.add("teston");
+      target.classList.add("estimation-selected");
     });
+
+    this.firstCatSecondTypeTargets.forEach((target) => {
+      target.classList.add("estimation-selected");
+    });
+
+    // remove color bad type
+
+
+    this.firstCatThirdTypeTargets.forEach((target) => {
+      target.classList.remove("estimation-selected");
+    });
+
   }
+
+  first_cat_third_choice() {
+
+    // add color good type
+
+    this.firstCatFirstTypeTargets.forEach((target) => {
+      target.classList.add("estimation-selected");
+    });
+
+    this.firstCatSecondTypeTargets.forEach((target) => {
+      target.classList.add("estimation-selected");
+    });
+
+    this.firstCatThirdTypeTargets.forEach((target) => {
+      target.classList.add("estimation-selected");
+    });
+
+  }
+
+
+
+
 }
