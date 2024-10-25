3439e170fbb83f7fcf3b4e39c7aaaacb745420c4
dorian fiolet <dorian3012@hotmail.fr>
2024-10-25 23:20:17 +1000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'nosservices', to: 'pages#nosservices'
-  get 'projects', to: 'pages#projects'
+  get 'projectx', to: 'pages#projectx'
   get 'estimation', to: 'pages#estimation'
   get 'about', to: 'pages#about'
   get 'test', to: 'pages#test'
 
   post 'send_contact', to: 'contacts#create'
+  resources :projects
   resources :contacts, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
