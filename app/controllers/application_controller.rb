3e712b5901cdc6229bb31b747d396db44b2693d0
dorian fiolet <dorian3012@hotmail.fr>
2024-12-13 16:56:27 +0100

    save the working translate mini starting.. now need find all the sentence for all the website <3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || extract_locale_from_header || I18n.default_locale
+  end
+
+  def extract_locale_from_header
+    locale = request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first
+    I18n.available_locales.map(&:to_s).include?(locale) ? locale : nil
+  end
+
+  def default_url_options
+    { locale: I18n.locale }.merge(super)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "pages#home"
-  get 'nosservices', to: 'pages#nosservices'
-  get 'projectx', to: 'pages#projectx'
-  get 'estimation', to: 'pages#estimation'
-  get 'about', to: 'pages#about'
-  get 'dashboard', to: 'pages#dashboard'
 
-  post 'send_contact', to: 'contacts#create'
-  resources :projects
-  resources :contacts, only: [:create]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|fr/ do
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    devise_for :users
+    root to: "pages#home"
+
+    get 'nosservices', to: 'pages#nosservices'
+    get 'projectx', to: 'pages#projectx'
+    get 'estimation', to: 'pages#estimation'
+    get 'about', to: 'pages#about'
+    get 'dashboard', to: 'pages#dashboard'
+
+    post 'send_contact', to: 'contacts#create'
+    resources :projects
+    resources :contacts, only: [:create]
+
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+    # Defines the root path route ("/")
+    # root "articles#index"
+  end
+
 end
