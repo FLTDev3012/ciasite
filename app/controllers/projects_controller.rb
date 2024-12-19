0485f4bb3062c268f55f5113877f40c7be04bec0
dorian fiolet <dorian3012@hotmail.fr>
2024-12-19 02:47:25 +0100

    index project order asc

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @projects = Project.order(created_at: :desc)
+    @projects = Project.order(created_at: :asc)
   end
 
   def show
